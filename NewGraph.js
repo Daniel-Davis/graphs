e3f9594a1ca18622111ac4f3bbc22a026a3eedcc
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-06 12:29:16 -0600

    Create NewGraph.js

---
+++ NewGraph.js
@@ -0,0 +1,7 @@
+// Daniel Davis, Graph, just a code for making a graph because capouch//
+
+load('graph.js'); // loads up graph info
+
+g = new graph(9); // new graph with 9 vertices
+g.addEdge(0,1);
+g.addEdge(0,3);

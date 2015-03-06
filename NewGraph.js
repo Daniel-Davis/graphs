d4154a3c70e293e6d95482f126ea24d3e6b6a855
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-06 12:31:42 -0600

    Update NewGraph.js

--- NewGraph.js
+++ NewGraph.js
@@ -2,6 +2,14 @@
 
 load('graph.js'); // loads up graph info
 
-g = new graph(9); // new graph with 9 vertices
+g = new graph(9); // new graph with 9 vertices and 10 edges
 g.addEdge(0,1);
 g.addEdge(0,3);
+g.addEdge(1,2);
+g.addEdge(1,3);
+g.addEdge(2,4);
+g.addEdge(3,4);
+g.addEdge(4,6);
+g.addEdge(4,7);
+g.addEdge(5,7);
+g.addEdge(7,8);
